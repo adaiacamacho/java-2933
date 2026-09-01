62d9dc70ade491c58b2c83b9b8eb143bc8e4ae1b
'Javier <'javier@eskura21.com'>
2026-09-01 10:43:44 +0200

    Capa de acceso a datos

---
+++ FULLSTACK/ipartube/src/main/java/com/ipartube/accesodatos/VideoCrud.java
@@ -0,0 +1,29 @@
+package com.ipartube.accesodatos;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import com.ipartube.dtos.Video;
+
+import bibliotecas.accesodatos.BaseDeDatos;
+
+public class VideoCrud {
+	public static ArrayList<Video> obtenerTodos() {
+		try (PreparedStatement pst = BaseDeDatos.crearSentencia("SELECT * FROM videos");
+				ResultSet rs = pst.executeQuery()) {
+			ArrayList<Video> videos = new ArrayList<Video>();
+
+			while (rs.next()) {
+				Video video = new Video(rs.getLong("id"), rs.getDate("fecha").toLocalDate(), rs.getString("url"),
+						rs.getString("titulo"), rs.getString("descripcion"));
+				videos.add(video);
+			}
+
+			return videos;
+		} catch (SQLException e) {
+			throw new RuntimeException("Error al obtener los videos", e);
+		}
+	}
+}
