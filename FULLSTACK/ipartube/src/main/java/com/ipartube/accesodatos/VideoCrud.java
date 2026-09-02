a7f33d90f2f31905643d5ad170847ae99116c51a
'Javier <'javier@eskura21.com'>
2026-09-02 10:09:09 +0200

    Implementación real del detalle de video

--- FULLSTACK/ipartube/src/main/java/com/ipartube/accesodatos/VideoCrud.java
+++ FULLSTACK/ipartube/src/main/java/com/ipartube/accesodatos/VideoCrud.java
@@ -3,7 +3,6 @@ package com.ipartube.accesodatos;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.LocalDate;
 import java.util.ArrayList;
 
 import com.ipartube.dtos.Video;
@@ -29,7 +28,21 @@ public class VideoCrud {
 	}
 
 	public static Video obtenerPorId(Long id) {
-		return new Video(1L, LocalDate.now(), "https://www.youtube.com/embed/fLexgOxsZu0", "Video de Bruno",
-				"Descripción del video");
+		try (PreparedStatement pst = BaseDeDatos.crearSentencia("SELECT * FROM videos WHERE id=?")) {
+			pst.setLong(1, id);
+			
+			ResultSet rs = pst.executeQuery();
+			
+			Video video = null;
+			
+			if (rs.next()) {
+				video = new Video(rs.getLong("id"), rs.getDate("fecha").toLocalDate(), rs.getString("url"),
+						rs.getString("titulo"), rs.getString("descripcion"));
+			}
+
+			return video;
+		} catch (SQLException e) {
+			throw new RuntimeException("Error al obtener el video " + id, e);
+		}
 	}
 }
