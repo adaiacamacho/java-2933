33a95b59d0cdfd892d0dab895ac329d499b6e455
'Javier <'javier@eskura21.com'>
2026-09-02 10:20:55 +0200

    Navegabilidad en Frontend en el menú para mostrar listado

--- FULLSTACK/ipartube/src/main/webapp/js/index.js
+++ FULLSTACK/ipartube/src/main/webapp/js/index.js
@@ -1,16 +1,17 @@
 const URL = 'api/v1/videos/';
 
-mostrar('listado');
+window.listado = async function() {
+    const listadoVideos = document.querySelector('#listado-videos');
 
-const listadoVideos = document.querySelector('#listado-videos');
+    const respuesta = await fetch(URL);
+    const videos = await respuesta.json();
 
-const respuesta = await fetch(URL);
-const videos = await respuesta.json();
-
-for (const video of videos) {
-    const card = document.createElement('div');
-    card.className = 'col';
-    card.innerHTML = `	
+	listadoVideos.innerHTML = '';
+	
+    for (const video of videos) {
+        const card = document.createElement('div');
+        card.className = 'col';
+        card.innerHTML = `	
 		<div class="card h-100">
 			<div class="ratio ratio-16x9 card-img-top">
 			  	<iframe src="${video.url}" title="${video.titulo}" allowfullscreen></iframe>
@@ -25,28 +26,33 @@ for (const video of videos) {
 		</div>
 	`;
 
-    listadoVideos.appendChild(card);
+        listadoVideos.appendChild(card);
+    }
+	
+	mostrar('listado');
 }
 
 function mostrar(id) {
-	const sections = document.querySelectorAll('main>section');
-	
-	for(const section of sections) {
-		section.style.display = 'none';
-	}
-	
-	document.querySelector('#' + id).style.display = null;
+    const sections = document.querySelectorAll('main>section');
+
+    for (const section of sections) {
+        section.style.display = 'none';
+    }
+
+    document.querySelector('#' + id).style.display = null;
 }
 
 window.detalle = async function(id) {
-	const respuesta = await fetch(URL + id);
-	const video = await respuesta.json();
-	
-	document.querySelector('#detalle iframe').src = video.url; // 'https://www.youtube.com/embed/fLexgOxsZu0';
-	document.querySelector('#detalle iframe').title = video.titulo; //'Video de Bruno Mars';
-	document.querySelector('#detalle .card-title').textContent = video.titulo; //'Video de Bruno Mars';
-	document.querySelector('#detalle .card-text').textContent = video.descripcion; // 'Bla bla bla';
-	document.querySelector('#detalle .card-footer small').textContent = video.fecha; //'FECHA';
-	
-	mostrar('detalle');
+    const respuesta = await fetch(URL + id);
+    const video = await respuesta.json();
+
+    document.querySelector('#detalle iframe').src = video.url; // 'https://www.youtube.com/embed/fLexgOxsZu0';
+    document.querySelector('#detalle iframe').title = video.titulo; //'Video de Bruno Mars';
+    document.querySelector('#detalle .card-title').textContent = video.titulo; //'Video de Bruno Mars';
+    document.querySelector('#detalle .card-text').textContent = video.descripcion; // 'Bla bla bla';
+    document.querySelector('#detalle .card-footer small').textContent = video.fecha; //'FECHA';
+
+    mostrar('detalle');
 }
+
+window.detalle(2);
