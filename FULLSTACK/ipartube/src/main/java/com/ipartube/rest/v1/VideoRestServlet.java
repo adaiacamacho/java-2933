b2c2d1aa4e77a97b0f83036ea79767c046cbdd6f
'Javier <'javier@eskura21.com'>
2026-09-02 08:45:30 +0200

    Cambio de serialización de GSON para LocalDate

--- FULLSTACK/ipartube/src/main/java/com/ipartube/rest/v1/VideoRestServlet.java
+++ FULLSTACK/ipartube/src/main/java/com/ipartube/rest/v1/VideoRestServlet.java
@@ -1,22 +1,36 @@
 package com.ipartube.rest.v1;
 
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializer;
+import com.ipartube.logicanegocio.AnonimoNegocio;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
-
-import com.google.gson.Gson;
-import com.ipartube.logicanegocio.AnonimoNegocio;
 
 @WebServlet("/videos/*")
 public class VideoRestServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-	private static final Gson GSON = new Gson();
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	private static final JsonSerializer<LocalDate> SER = (src, type,
+			ctx) -> new JsonPrimitive(src.format(DateTimeFormatter.ISO_LOCAL_DATE));
+
+	private static final JsonDeserializer<LocalDate> DESER = (json, type, ctx) -> LocalDate.parse(json.getAsString());
+
+	private static final Gson GSON = new GsonBuilder().registerTypeAdapter(LocalDate.class, SER)
+			.registerTypeAdapter(LocalDate.class, DESER).create();
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		response.setContentType("application/json");
 		response.getWriter().append(GSON.toJson(AnonimoNegocio.listarVideos()));
 	}
