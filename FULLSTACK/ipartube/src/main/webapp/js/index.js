8e11f67e1adddf1061e8e950b9c88543bdf36d49
'Javier <'javier@eskura21.com'>
2026-09-02 09:44:51 +0200

    Simulacro en Frontend de vista de detalle

--- FULLSTACK/ipartube/src/main/webapp/js/index.js
+++ FULLSTACK/ipartube/src/main/webapp/js/index.js
@@ -1,20 +1,22 @@
 const URL = 'api/v1/videos/';
 
+mostrar('listado');
+
 const listadoVideos = document.querySelector('#listado-videos');
 
 const respuesta = await fetch(URL);
 const videos = await respuesta.json();
 
-for(const video of videos) {
-	const card = document.createElement('div');
-	card.className = 'col';
-	card.innerHTML = `	
+for (const video of videos) {
+    const card = document.createElement('div');
+    card.className = 'col';
+    card.innerHTML = `	
 		<div class="card h-100">
 			<div class="ratio ratio-16x9 card-img-top">
 			  	<iframe src="${video.url}" title="${video.titulo}" allowfullscreen></iframe>
 			</div>
 			<div class="card-body">
-				<h5 class="card-title">${video.titulo}</h5>
+				<h5 class="card-title"><a href="javascript:detalle()">${video.titulo}</a></h5>
 				<p class="card-text">${video.descripcion}</p>
 			</div>
 			<div class="card-footer">
@@ -22,6 +24,25 @@ for(const video of videos) {
 			</div>
 		</div>
 	`;
+
+    listadoVideos.appendChild(card);
+}
+
+function mostrar(id) {
+	const sections = document.querySelectorAll('main>section');
 	
-	listadoVideos.appendChild(card);
+	for(const section of sections) {
+		section.style.display = 'none';
+	}
+	
+	document.querySelector('#' + id).style.display = null;
+}
+
+window.detalle = function() {
+	document.querySelector('#detalle iframe').src = 'https://www.youtube.com/embed/fLexgOxsZu0';
+	document.querySelector('#detalle .card-title').textContent = 'Video de Bruno Mars';
+	document.querySelector('#detalle .card-text').textContent = 'Bla bla bla';
+	document.querySelector('#detalle .card-footer small').textContent = 'FECHA';
+	
+	mostrar('detalle');
 }
