17039295e97e7df7318b16074cdeffb1701ff916
'Javier <'javier@eskura21.com'>
2026-09-15 10:52:41 +0200

    Declaramos las características globales que puede tener cualquier DAO

---
+++ FULLSTACK/ipartex/src/main/java/com/ipartex/accesodatos/Dao.java
@@ -0,0 +1,25 @@
+package com.ipartex.accesodatos;
+
+import java.util.Optional;
+
+public interface Dao<T> {
+	default Iterable<T> obtenerTodos() {
+		throw new AccesoDatosException("NO IMPLEMENTADO");
+	}
+
+	default Optional<T> obtenerPorId(Long id) {
+		throw new AccesoDatosException("NO IMPLEMENTADO");
+	}
+
+	default T insertar(T objeto) {
+		throw new AccesoDatosException("NO IMPLEMENTADO");
+	}
+
+	default T modificar(T objeto) {
+		throw new AccesoDatosException("NO IMPLEMENTADO");
+	}
+
+	default void borrar(Long id) {
+		throw new AccesoDatosException("NO IMPLEMENTADO");
+	}
+}
