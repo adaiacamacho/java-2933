953ecd09178180682cea6b06c3845194c8e41c1e
'Javier <'javier@eskura21.com'>
2026-09-23 09:53:02 +0200

    USUARIO: definimos operaciones de negocio

---
+++ FULLSTACK/ipartex/src/main/java/com/ipartex/logicanegocio/AdministradorNegocio.java
@@ -0,0 +1,17 @@
+package com.ipartex.logicanegocio;
+
+import java.util.Optional;
+
+import com.ipartex.entidades.Usuario;
+
+public interface AdministradorNegocio {
+	Iterable<Usuario> obtenerListadoUsuarios();
+
+	Optional<Usuario> obtenerUsuarioPorId(Long id);
+
+	Usuario crearUsuario(Usuario usuario);
+
+	Usuario modificarUsuario(Usuario usuario);
+
+	void borrarUsuario(Long id);
+}
